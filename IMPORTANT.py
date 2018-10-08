fe6a96de6ee160d0a799636c88e27640b2992ff4
Eugenio Fonzi <eugenio.fonzi2@str957-bio1.personale.dir.unibo.it>
2018-10-08 11:51:51 +0200

    add lxml to py365_euge

--- IMPORTANT.py
+++ IMPORTANT.py
@@ -79,6 +79,7 @@ conda install -y -n py365_euge biopython
 conda install -y -n py365_euge rpy2
 conda install -y -n py365_euge xlrd
 conda install -y -n py365_euge snakemake
+conda install -y -n py365_euge lxml
 source activate py365_euge
 python -m ipykernel install --user --name py365_euge --display-name 'py365_euge'
 
